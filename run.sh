3b7d4681eacceaf045796307673236c9ab8aa20e
Srini-98 <srini481@gmail.com>
2024-02-26 10:02:03 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 
-model_path=
-learning_rate=
-dataset_path=
-output_dir=
-model_name=
+model_path=$1
+learning_rate=$2
+dataset_path=$3
+output_dir=$4
+model_name=$5
 
 nohup torchrun --nnodes=1 --nproc-per-node=4 --rdzv-backend=c10d --rdzv-endpoint=localhost:0 train.py \
     --model_path $model_path \
