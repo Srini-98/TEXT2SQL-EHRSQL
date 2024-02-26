3afd2841f4fe304a2afa592b020207d580452399
Srini-98 <srini481@gmail.com>
2024-02-26 10:05:29 +0800

    run multi

---
+++ run_multi.sh
@@ -0,0 +1,13 @@
+
+model_path=$1
+learning_rate=$2
+dataset_path=$3
+output_dir=$4
+model_name=$5
+
+nohup torchrun --nnodes=1 --nproc-per-node=4 --rdzv-backend=c10d --rdzv-endpoint=localhost:0 train_multifunction.py \
+    --model_path $model_path \
+    --learning_rate $learning_rate \
+    --dataset_path $dataset_path \
+    --output_dir $output_dir \
+    --model_name $model_name > fsdp_logs.txt
