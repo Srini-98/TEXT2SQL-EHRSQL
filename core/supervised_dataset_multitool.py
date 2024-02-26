12b6adef7e1ae2aa840181c7ca2f07e3412a9dc1
Srini-98 <srini481@gmail.com>
2024-02-26 09:07:59 +0800

    inference and prompts

---
+++ core/supervised_dataset_multitool.py
@@ -0,0 +1,206 @@
+from dataclasses import dataclass
+import os 
+from typing import List, Optional, Tuple, Union , Dict , Sequence
+import datasets 
+import logging 
+import torch.distributed as dist
+import torch 
+import transformers
+import copy 
+import math
+import ast
+from torch.utils.data import Dataset
+
+IGNORE_INDEX = -100
+DEFAULT_PAD_TOKEN = "<|pad|>"
+DEFAULT_EOS_TOKEN = "<|endoftext|>"
+DEFAULT_UNK_TOKEN = "<|unk|>"
+table_lis = []
+
+with open("./prompts/main_multi_tool_prompt_input.txt", "r") as f:
+    prompt_input = f.read()
+
+with open("./prompts/main_multi_tool_prompt_output.txt", "r") as g:
+    prompt_output = g.read()
+
+with open("./prompts/tables_eicu.txt" , "r") as g:
+    table_format = g.read().split("\n\n")
+
+# with open("./foreign_keys.txt" , "r") as h:
+#     foreign_keys = h.read().split(",\n")
+
+
+for i in table_format:
+    t = i.split("(")[0].split("TABLE ")[1]
+    table_lis.append(t.lower())
+
+# print(table_lis)    
+# def format_output(function_lis , schema ,code , prompt_output):
+#     return prompt_output.format(function_lis=function_lis , schema="\n\n".join(schema.split("\n\n"))[0:-1] , code=code)
+#                                 #"\n\n".join(st['table_query'].split("\n\n"))[0:-1]  , code=code)
+#                                 #st['query'])
+    
+# def format_input(question , prompt , table_format , foreign_keys):
+#     return prompt.format(tables_list=table_format , foreign_keys = " , ".join(foreign_keys) , question = question)
+
+
+def format_input(question , prompt , table_format):
+    return prompt.format(tables_list = table_format , question = question)
+
+def format_output(function_lis , schema , code , get_keys ,  prompt_output , foreign_keys):
+    key_fn = ""
+    if get_keys == "[]":
+        key_fn = ""
+    else:
+        key_fn = f" get_keys({function_lis})"
+    
+    print("get keys: " , key_fn)
+    
+    if foreign_keys == "[]":
+        foreign_keys = "Not Required"
+    else:
+        foreign_keys = " , ".join(ast.literal_eval(foreign_keys))
+    return prompt_output.format(function_lis = function_lis , get_keys = key_fn , schema = schema , code = code , foreign_keys = foreign_keys)
+
+
+def _tokenize_fn(strings: Sequence[str] , tokenizer: transformers.PreTrainedTokenizer) ->Dict:
+    tokenized_list = [tokenizer(text , return_tensors="pt" , padding=False , max_length=tokenizer.model_max_length , truncation=True) for text in strings]
+    input_ids = labels = [tokenized.input_ids[0] for tokenized in tokenized_list]
+    input_id_lens = labels_lens = [
+        tokenized.input_ids.ne(tokenizer.pad_token_id).sum().item() for tokenized in tokenized_list
+    ]
+
+    return dict(
+        input_ids = input_ids,
+        labels = labels,
+        input_id_lens = input_id_lens,
+        labels_lens = labels_lens,
+    )
+
+def preprocess(train_on_inputs: bool , samples: Sequence[str] , tokenizer: transformers.PreTrainedTokenizer) -> Dict:
+    sources = [f"{format_input(question=st , prompt=prompt_input , table_format=table_lis)}" for st in samples['question']]
+    targets = [f"{format_output(function_lis=function_lis , schema=schema , code=code , prompt_output=prompt_output , get_keys = get_keys , foreign_keys = f_keys)}{tokenizer.eos_token}" for function_lis , schema , code , get_keys , f_keys in zip( samples['table_lis'] , samples['table_query'],samples['query'] , samples['foreign_keys_val'] , samples['foreign_keys_val'])]
+    examples = [s + t for s , t in zip(sources , targets)]
+    examples_tokenized , source_tokenized = [ _tokenize_fn(strings , tokenizer) for strings in (examples , sources)]
+    input_ids = examples_tokenized['input_ids']
+    labels = copy.deepcopy(input_ids)
+    for label , source_len in zip(labels , source_tokenized['input_id_lens']):
+        if not train_on_inputs:
+            label[:source_len] = IGNORE_INDEX
+
+    final_l = []
+    for val in labels:
+        res = []
+        i = 0
+        s = 0
+        while i < len(val):
+            if val[i] == 13 and val[i+1] == 13:
+                break
+            res.append(val[i].item())
+            i = i + 1
+
+        res.append(-100)
+        res.append(-100)
+        i = i + 2
+        while i < len(val):
+            if val[i] == 13 and val[i+1] == 13 and val[i+2] == 13:
+                break
+            res.append(-100)
+            i = i + 1
+
+        res.append(-100)
+        res.append(-100)
+        res.append(-100)
+        i = i + 3
+
+
+        while i < len(val):
+            if val[i] == 13 and val[i+1] == 13:
+                break
+            res.append(-100)
+            i = i + 1
+        
+        res.append(-100)
+        res.append(-100)
+        i = i + 2
+        
+        while i < len(val):
+            res.append(val[i].item())
+            i = i + 1
+        
+        final_l.append(torch.tensor(res))
+
+    return dict(input_ids = input_ids , labels = final_l)
+
+def _filter_tokenize_fn(strings: Sequence[str] , tokenizer: transformers.PreTrainedTokenizer) -> Dict:
+    samples = []
+    for text in strings:
+        tokens = tokenizer(text , return_tensors="pt" , padding=False , 
+                           max_length=tokenizer.model_max_length , truncation=True)
+
+        if tokens.input_ids.squeeze().numel() < tokenizer.model_max_length:
+            samples.append(True)
+        else:
+            samples.append(False)
+    return samples
+
+def filter_long_samples(samples: Sequence[str] , tokenizer: transformers.PreTrainedTokenizer) -> Dict:
+    sources = [f"{format_input(question=st , prompt=prompt_input , table_format=table_lis)}" for st in samples['question']]
+    targets = [f"{format_output(function_lis=function_lis , schema=schema , code=code , prompt_output=prompt_output , get_keys = get_keys , foreign_keys = f_keys)}{tokenizer.eos_token}" for function_lis , schema , code , get_keys , f_keys in zip( samples['table_lis'] , samples['table_query'],samples['query'] , samples['foreign_keys_val'] , samples['foreign_keys_val'])]
+    examples = [s + t for s , t in zip(sources , targets)]
+    return _filter_tokenize_fn(examples , tokenizer)
+
+
+class SuperVisedDataset(Dataset):
+
+    def __init__(self , train_on_inputs: bool , tokenizer: transformers.PreTrainedTokenizer , dataset):
+
+        super(SuperVisedDataset , self).__init__()
+        workers = math.ceil(os.cpu_count() / dist.get_world_size())
+        logging.warning(f"Tokenizing with {workers} workers")
+        dataset = dataset.filter(
+            lambda x: filter_long_samples(x , tokenizer) ,
+            batched=True,
+            batch_size=1000,
+            num_proc=workers
+        ).map(
+            lambda x: preprocess(train_on_inputs , x , tokenizer),
+            batched=True,
+            batch_size=1000,
+            num_proc=workers,
+        )
+        
+        self.input_ids = dataset['input_ids']
+        self.labels = dataset['labels']
+    
+    def __len__(self):
+        return len(self.input_ids)
+    
+    def __getitem__(self , idx) -> Dict[str , torch.Tensor]:
+        return dict(
+            input_ids = torch.tensor(self.input_ids[idx]),
+            labels = torch.tensor(self.labels[idx]),
+        )
+    
+@dataclass
+class DataCollatorForSuperVisedDataset(object):
+    """collate examples for supervised finetuning"""
+
+    tokenizer: transformers.PreTrainedTokenizer
+
+    def __call__(self , instances: Sequence[Dict]) -> Dict[str , torch.Tensor]:
+        input_ids , labels = tuple(
+            [instance[key] for instance in instances] for key in ("input_ids" , "labels")
+        )
+        input_ids = torch.nn.utils.rnn.pad_sequence(
+            input_ids , batch_first=True , padding_value=self.tokenizer.pad_token_id
+        )
+        labels = torch.nn.utils.rnn.pad_sequence(
+            labels , batch_first=True , padding_value=IGNORE_INDEX
+        )
+
+        return dict(
+            input_ids = input_ids,
+            labels = labels,
+            attention_mask = input_ids.ne(self.tokenizer.pad_token_id)
+        )
---
+++ inference/vllm_multi_tool_inference.py
@@ -0,0 +1,283 @@
+import fire
+import json
+import torch
+from vllm import LLM
+from vllm import LLM, SamplingParams
+from datasets import load_dataset
+import torch
+import time
+from tqdm import tqdm
+from datasets import load_from_disk
+import ast
+import json
+
+torch.cuda.manual_seed(42)
+torch.manual_seed(42)
+table_lis = []
+
+with open("./prompts/main_multi_tool_prompt_input.txt", "r") as f:
+    prompt_input = f.read()
+
+with open("./prompts/main_multi_tool_prompt_output.txt", "r") as g:
+    prompt_output = g.read()
+
+
+
+def get_foreign_keys(keys_lis , tup_lis , foreign_keys):
+    st = []
+    for table_pair in tup_lis:
+        if table_pair[0].lower().strip() in keys_lis and table_pair[1].lower().strip() in keys_lis: 
+            st.append(foreign_keys[tup_lis.index(table_pair)])
+    return st
+    
+
+def format_input(question , prompt , table_format):
+    return prompt.format(tables_list = table_format , question = question)
+
+def format_input_code(question , prompt , foreign_keys ,  table_format , fn_call ,  table_json , tup_lis):
+    st = prompt.format(tables_list=table_format , question = question)
+    st = st + fn_call + "\n\n" + "Table schema:\n"
+    lis = fn_call.split("get_schema(")[1].split(")")[0]
+    if "get_keys" in fn_call:
+        keys_lis = fn_call.split("get_keys(")[1].split(")")[0] 
+        keys_lis = ast.literal_eval(keys_lis)
+        if len(keys_lis) == 1:
+            foreign_keys = "Not Required"
+        else:
+            foreign_keys = get_foreign_keys(keys_lis , tup_lis , foreign_keys)
+            foreign_keys = " , ".join(foreign_keys)
+            if foreign_keys == "":
+                foreign_keys = "Not Required"
+    else:
+        foreign_keys = "Not Required"
+    actual_list = ast.literal_eval(lis)
+    table_st = ""
+    
+    for j in actual_list:
+        try:
+            table_json_value = table_json[j]
+            table_st = table_st +  table_json[j] + "\n\n"
+        except:
+            print("Illegal table" , j)
+            continue
+
+    st = st + table_st + "\n" + "Foreign Keys:\n" + foreign_keys + "\n\n" 
+
+    return st
+
+    
+
+
+def load_model(model_name, tp_size=1):
+    llm = LLM(model_name, tensor_parallel_size=tp_size)
+    return llm
+
+def main(
+    model,
+    max_new_tokens=300,
+    user_prompt=None,
+    top_p=0,
+    dataset_path = None,
+    temperature=0,
+    output_path = None,
+    data_used = None , 
+    valid_original = None , 
+    table_st = None , 
+    foreign_st = None ,
+    inference_stage = None , 
+    table_func_file = None , 
+    pred_table_path = None , 
+    batch_size=6
+):
+    print("dataset used for eval" , data_used)
+    print("output_file" , output_path)
+    print("valid original" , valid_original)
+    print("table st" , table_st)
+    print("foreign st" , foreign_st)
+    print("inference stage" , inference_stage)
+    print("table func file" , table_func_file)
+
+    with open(f"./prompts/{table_st}" , "r") as g:
+        table_format = g.read().split("\n\n")
+
+    with open(f"./prompts/{foreign_st}" , "r") as h:
+        foreign_keys = h.read().split(",\n")
+
+    with open(f"./mimic_data/{table_func_file}" , "r") as f:
+        table_json = json.load(f)
+    
+
+    table_lis = []
+    for i in table_format:
+        t = i.split("(")[0].split("TABLE ")[1]
+        table_lis.append(t.lower())
+    
+    f_lis = []
+    tup_lis = []
+    for i in foreign_keys:
+        f_lis.append(i.lower())
+        t_1 = i.split("=")[0].split(".")[0]
+        t_2 = i.split("=")[1].split(".")[0]
+        tup_lis.append([t_1,t_2])
+
+
+    if inference_stage == 2:
+        with open(pred_table_path , "r") as t:
+            predicted_tables = t.read().split("\n")
+    
+    with open(valid_original , "r") as g:
+        data_original = json.load(g)
+    
+    label_lis = []
+    impossible_lis = []
+    db_id = []
+    id = []
+    question_lis = []
+    for i in data_original:
+        question_lis.append(i['question'])
+        label_lis.append(i['query'])
+        impossible_lis.append(i["is_impossible"])
+        db_id.append(i["db_id"])
+        id.append(i["id"]) 
+    
+    print("number of questions" , len(question_lis))
+    data = load_dataset("json", data_files= dataset_path)
+    print(data)
+    out_eval = {}
+    start_ind = 0
+    
+    if inference_stage == 1:
+        sampling_param = SamplingParams(max_tokens=600 , temperature=0 ,logprobs=32000 , stop = ["</function_call>"]) 
+    else: 
+        sampling_param = SamplingParams(max_tokens=600 , temperature=0 ,logprobs=32000)
+    
+    start_time = time.time()
+    j = 0
+    flag = 0
+    if inference_stage == 1:
+        output_file = open(f"{output_path}" , "w")
+
+    out_eval = {}
+    num_beams=1
+    start_ind = 0
+    print("batch size is" , batch_size)
+    for i in range(0 , len(data['train']) , batch_size):
+        user_prompt = []
+        for k in range(0 , batch_size):
+            if data_used == "mimic3":
+                if inference_stage == 1:
+                    element = format_input(data['train'][i+k]['question'] , prompt_input , table_lis) if i + k < len(data['train']) else None
+                else:
+                    element = format_input_code(question=data['train'][i+k]['question'] , foreign_keys=foreign_keys ,  prompt=prompt_input , table_format=table_lis , fn_call= predicted_tables[i+k], table_json=table_json , tup_lis = tup_lis) if i + k < len(data['train']) else None
+
+                #print(element)
+                if element is not None and element != "ERROR":
+                    user_prompt.append(element)
+                else:
+                    print("none element")
+                    print(element)
+                    # if inference_stage==1:
+                    #     output_file.write("ERROR- " + str(i)  + "\n--------------------------------------------\n")
+        
+
+        # if i == 0:
+        #     print(user_prompt[0])
+            
+        outputs = model.generate(user_prompt, sampling_params=sampling_param)
+        if data_used == "mimic3":
+            for ct , output in enumerate(outputs):
+                output_lis = []
+                for o in output.outputs:
+                    final_output = o.text
+                    output_lis.append(final_output)
+
+                    if inference_stage == 1:
+                        print("final output" , final_output)
+                        output_file.write(final_output + "</function_call>" + "\n")
+                    else:
+                        try:
+                            code = final_output.split("SQLCODE:\n")[1]
+                        except:
+                            print("final output due to ERROR" , element + final_output)
+                            code = "ERROR"
+
+                        print("final output" , code)
+                        #output_file.write(user_prompt[ct] + final_output + "\n--------------------------------------------\n")
+
+                        log_probs = output.outputs[0].logprobs
+                        final_scores = []
+                        t = ()
+                        for log_token in log_probs:
+                            lis = []
+                            scores = []
+                            for tok , val in log_token.items():
+                                lis.append(val)
+                            scores.append(lis)
+                            t = t + (torch.tensor(scores),)
+
+                        logits = torch.stack(t, dim=1)[::int(num_beams/1)]
+                        logits = logits.cpu()
+                        output_prob = torch.softmax(logits, dim=2).float()
+                        log_prob = torch.log_softmax(logits, dim=2).float()
+                        output_prob = torch.softmax(logits, dim=2).float()
+                        log_prob = torch.log_softmax(logits, dim=2).float()
+                        sequences_entropy = (torch.sum(output_prob * log_prob, dim=2) * (-1) ).numpy()
+                        result = {}
+                        result['question'] = question_lis[start_ind]
+                        result['real'] = label_lis[start_ind]
+                        result['pred'] = code
+                        entropy = sequences_entropy[0].tolist()
+                        result['sequence_entropy'] = tuple(entropy)
+                        result['db_id'] = db_id[start_ind]
+                        result['is_impossible'] =  impossible_lis[start_ind]
+                        out_eval[id[start_ind]] = result
+                        start_ind = start_ind + 1
+
+                if i == 0 and flag == 0:
+                    flag = 1
+                    print("output is")
+                    print(user_prompt[0] + final_output)
+                print("-------------------------------------")
+    
+    if inference_stage == 2:
+        with open(output_path , "w" ) as f:
+            json.dump(out_eval , f)
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    if inference_stage == 1:
+        output_file.close()
+    print(f"Execution time: {elapsed_time:.2f} seconds")
+
+    
+def run_script(
+    model_name: str,
+    peft_model=None,
+    tp_size=1,
+    max_new_tokens=100,
+    batch_size=6,
+    user_prompt=None,
+    top_p=0.9,
+    temperature=0.8,
+    dataset_path = None,
+    output_path = None,
+    data_used = None , 
+    valid_original = None , 
+    table_st = None , 
+    foreign_st = None , 
+    inference_stage = None ,
+    table_func_file = None , 
+    pred_table_path = None , 
+):
+    print("starting to load model")
+    print('tp_size' , tp_size)
+    print("batch size" , batch_size)
+    model = load_model(model_name, tp_size)
+    print("Finished loading model")
+
+    print(len(model.get_tokenizer()))
+    main(model = model, max_new_tokens= int(max_new_tokens), user_prompt= user_prompt, top_p= int(top_p), temperature= int(temperature) , 
+         dataset_path=dataset_path , output_path = output_path , data_used = data_used , valid_original = valid_original , table_st = table_st , foreign_st = foreign_st , inference_stage = inference_stage ,
+        table_func_file = table_func_file , pred_table_path = pred_table_path , batch_size= batch_size)
+
+if __name__ == "__main__":
+    fire.Fire(run_script)
